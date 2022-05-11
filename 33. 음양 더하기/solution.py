b8879d098ac91850dcb749b2cdeb11d6bf2b65b4
93bulldozer <fdmin930905@gmail.com>
2022-05-12 01:05:22 +0900

    음양 더하기

---
+++ 33. 음양 더하기/solution.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for absolute, sign in zip(absolutes, signs):
+        if sign == True:
+            answer += absolute
+        else:
+            answer -= absolute
+    return answer
+            
